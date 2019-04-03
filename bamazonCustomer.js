715b6288d19c00b145c54da5004d6203160ee447
Phi Hai Nguyen <phihai93@gmail.com>
2019-04-02 17:19:13 -0700

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -60,28 +60,23 @@ function buyProduct() {
       console.log(answer.productID);
       connection.query("SELECT * FROM products WHERE id =?", [answer.productID], function (err, res) {
         for (let i = 0; i < res.length; i++) {
-          console.log("item ID: " + res[i].id + " || " + res[i].name + " || " + " Quantity: " + answer.quantity, res[i].quantity + "|| total: " + answer.quantity * res[i].price)
+          console.log("item ID: " + res[i].id + " || " + res[i].name + " || " + " Quantity: " + answer.quantity+" ||  Quantity Remaining: " + res[i].quantity + "|| total: " + answer.quantity * res[i].price)
           if (res[i].quantity >= answer.quantity) {
-            console.log(true);
-            console.log(res)
+            // console.log(true);
+            // console.log(res)
             console.log("remaining " + res[i].name + ": " + (res[i].quantity - Number([answer.quantity])))
-            var updateInventory = res[i].quantity - Number([answer.quantity])
+             var updateInventory = res[i].quantity - Number([answer.quantity])
             totalBill.push(answer.quantity * res[i].price)
+            
             function update() {
               var query = connection.query(
                 "UPDATE products SET ? WHERE ?", [
-                  {
-                    quantity: updateInventory,
-                  },
-                  {
-                    id: answer.productID,
-                  }
+                  {quantity: updateInventory,},
+                  {id: answer.productID,}
                 ]
-
-              )
-            }readProducts();
+              )}
             update();
-            decision();
+            pay();
           } else {
             console.log("Insufficient quantity!");
             buyProduct();
@@ -90,45 +85,48 @@ function buyProduct() {
       })
     })
 }
-function decision() {
-  inquirer
-    .prompt({
-      name: "verify",
-      type: "rawlist",
-      message: "What would you like to do?",
-      choices: [
-        "Continue shopping",
-        "Checkout"
-      ]
-    })
-    .then(function (answer) {
-      switch (answer.verify) {
-        case "Continue shopping":
-          buyProduct();
-          break;
+// function decision() {
+//   inquirer
+//     .prompt({
+//       name: "verify",
+//       type: "rawlist",
+//       message: "What would you like to do?",
+//       choices: [
+//         "Continue shopping",
+//         "Checkout"
+//       ]
+//     })
+//     .then(function (answer) {
+//       switch (answer.verify) {
+//         case "Continue shopping":
+//           buyProduct();
+//           break;
 
-        case "Checkout":
-          pay();
-          break;
-      }
-    });
-}
+//         case "Checkout":
+//           pay();
+//           break;
+//       }
+//     });
+// }
 function pay() {
-  inquirer
-    .prompt({
-      name: "payment",
-      type: "confirm",
-      message: "Are you sure you wish to checkout?"
-    })
-    .then(function confirmed() {
+  // inquirer
+  //   .prompt({
+  //     name: "payment",
+  //     type: "confirm",
+  //     message: "Are you sure you wish to checkout?"
+  //   })
+  //   .then(function confirmed() {
       console.log("Your total is $"+totalBill +" Thank You, come again!");
-    }, function cancelled() {
-      console.log("Ok checkout when you are ready");
-      buyProduct();
-    });
-}
+      readProducts();
+    }
+//     , 
+//     function cancelled() {
+//       console.log("Ok checkout when you are ready");
+//       buyProduct();
+//     });
+// }
 function readProducts() {
-  console.log("Selecting all products...\n");
+  console.log("\n");
   connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
     console.table(res);
